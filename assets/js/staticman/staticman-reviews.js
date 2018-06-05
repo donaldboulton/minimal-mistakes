146a21d320317107c5d75455cfc43d3320ca0857
Donald Boulton <donaldboulton@gmail.com>
2018-06-05 07:19:30 -0500

    fixing comments and reviews ,js

--- assets/js/staticman/staticman-reviews.js
+++ assets/js/staticman/staticman-reviews.js
@@ -1,7 +1,7 @@
 (function ($) {
-  var $comments = $('.js-comments');
+  var $comments = $('.js-reviews');
 
-  $('.js-form').submit(function () {
+  $('.js-form1').submit(function () {
     var form = this;
 
     $(form).addClass('form--loading');
@@ -12,27 +12,25 @@
       data: $(this).serialize(),
       contentType: 'application/x-www-form-urlencoded',
       success: function (data) {
-        showModal('Review submitted', 'Thanks for your review! It will show on the site once it has been approved. You can see the pull request <a href="https://github.com/eduardoboucas/popcorn/pulls">here</a>.');
-        $(form).removeClass('form--loading');
+        $('#review-form-submit').html('{{ site.data.ui-text[site.locale].review_btn_submitted | default: "Submitted" }}');
+        $('.page__reviews-form .js-notice').removeClass('notice--danger');
+        $('.page__reviews-form .js-notice').addClass('notice--success');
+        showAlert('{{ site.data.ui-text[site.locale].review_success_msg | default: "Thanks for your review! It will show on the site once it has been approved." }}');
       },
       error: function (err) {
         console.log(err);
-        showModal('Error', 'Sorry, there was an error with the submission!');
-        $(form).removeClass('form--loading');
+        $('#review-form-submit').html('{{ site.data.ui-text[site.locale].review_btn_submit  | default: "Submit Review" }}');
+        $('.page__reviews-form .js-notice').removeClass('notice--success');
+        $('.page__reviews-form .js-notice').addClass('notice--danger');
+        showAlert('{{ site.data.ui-text[site.locale].review_error_msg | default: "Sorry, there was an error with your submission. Please make sure all required fields have been completed and try again." }}');
+        $(form).removeClass('disabled');
       }
     });
 
     return false;
   });
-
-  $('.js-close-modal').click(function () {
-    $('body').removeClass('show-modal');
-  });
-
-  function showModal(title, message) {
-    $('.js-modal-title').text(title);
-    $('.js-modal-text').html(message);
-
-    $('body').addClass('show-modal');
+  function showAlert(message) {
+    $('.page__reviews-form .js-notice').removeClass('hidden');
+    $('.page__reviews-form .js-notice-text').html(message);
   }
 })(jQuery);
