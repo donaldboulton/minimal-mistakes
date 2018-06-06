400435774a8746f1de6b8e69f58a5237203358cd
Donald Boulton <donaldboulton@gmail.com>
2018-06-06 11:13:28 -0500

    page reviews

--- assets/js/staticman/staticman-reviews.js
+++ assets/js/staticman/staticman-reviews.js
@@ -15,17 +15,17 @@
       data: $(this).serialize(),
       contentType: 'application/x-www-form-urlencoded',
       success: function (data) {
-        $('#review-form-submit').html('{{ site.data.ui-text[site.locale].review_btn_submitted | default: "Submitted" }}');
+        $('#review-form-submit').html('{{ "Submitted" }}');
         $('.page__reviews-form .js-notice').removeClass('notice--danger');
         $('.page__reviews-form .js-notice').addClass('notice--success');
-        showAlert('{{ site.data.ui-text[site.locale].review_success_msg | default: "Thanks for your review! It will show on the site once it has been approved." }}');
+        showAlert('{{ "Thanks for your review! It will show on the site once it has been approved." }}');
       },
       error: function (err) {
         console.log(err);
-        $('#review-form-submit').html('{{ site.data.ui-text[site.locale].review_btn_submit  | default: "Submit Review" }}');
+        $('#review-form-submit').html('{{ "Submit Review" }}');
         $('.page__reviews-form .js-notice').removeClass('notice--success');
         $('.page__reviews-form .js-notice').addClass('notice--danger');
-        showAlert('{{ site.data.ui-text[site.locale].review_error_msg | default: "Sorry, there was an error with your submission. Please make sure all required fields have been completed and try again." }}');
+        showAlert('{{ "Sorry, there was an error with your submission. Please make sure all required fields have been completed and try again." }}');
         $(form).removeClass('disabled');
       }
     });
