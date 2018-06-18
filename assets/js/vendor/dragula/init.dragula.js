3c06cad04c296d4d0f1b2568df10eb6ab4dd2878
Donald Boulton <donaldboulton@gmail.com>
2018-06-18 14:39:36 -0500

    dragula in scripts

--- assets/js/vendor/dragula/init.dragula.js
+++ assets/js/vendor/dragula/init.dragula.js
@@ -4,11 +4,4 @@ function $(id) {
 
 dragula([$('drag-elements'), $('drop-target')], {
   revertOnSpill: true
-}).on('drop', function(el) {
-  if ($('drop-target').children.length > 0) {
-    $('post').innerHTML = $('drop-target').innerHTML;
-  } else {
-    $('post').innerHTML = "Post";
-  }
-
 });
