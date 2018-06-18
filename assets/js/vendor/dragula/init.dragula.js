673717686069c104a301c8d284bf9456576e04e8
Donald Boulton <donaldboulton@gmail.com>
2018-06-18 14:22:35 -0500

    test

--- assets/js/vendor/dragula/init.dragula.js
+++ assets/js/vendor/dragula/init.dragula.js
@@ -1,1 +1,14 @@
-dragula([$('left-defaults'), $('right-defaults')]);
+function $(id) {
+  return document.getElementById(id);
+}
+
+dragula([$('drag-elements'), $('drop-target')], {
+  revertOnSpill: true
+}).on('drop', function(el) {
+  if ($('drop-target').children.length > 0) {
+    $('post').innerHTML = $('drop-target').innerHTML;
+  } else {
+    $('post').innerHTML = "Post";
+  }
+
+});
