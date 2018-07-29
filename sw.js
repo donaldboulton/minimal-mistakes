95dbdd16cb2bcf8f688dbf046a921159cb4e7ac1
Donald Boulton <donaldboulton@gmail.com>
2018-07-28 19:08:30 -0500

    /

--- sw.js
+++ sw.js
@@ -1,32 +1,44 @@
 var urlsToCache = [
-  'offline.html',
+  '/offline.html',
   'assets/images/pages/bg-10-min.png',
+  'assets/images/fav-icons/favicon.png',
   'assets/css/main.css',
   'assets/js/main.min.js'
 ];
 
-var CACHE_NAME = 'don-boulton-cache-v1';
+var CACHE_NAME = 'donboulton-cache-v1';
 
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+          return cache.match(event.request).then(function (response) {
+              return response || fetch(event.request).then(function (response) {
+                  cache.put(event.request, response.clone());
+                  return response;
+              });
+          });
+      })
+  );
+});
+
 // Cache name: adjust version number to invalidate service worker cachce.
-var CACHE_NAME = 'don-boulton-cache-v2';
+var CACHE_NAME = 'donboulton-cache-v2';
 
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
