7644a4a04a137b2d0b1d5a26f913ea90b0268ed0
Donald Boulton <donaldboulton@gmail.com>
2018-07-30 15:12:14 -0500

    adding push notify api and sw ssets

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ layout: null
 'use strict'
 
 var APP_PREFIX = 'donboulton';
-var VERSION = 'version_01';
+var VERSION = 'version_02';
 var CACHE_NAME = APP_PREFIX + VERSION;
 var URLS = [
   '/',
@@ -26,6 +26,40 @@ var URLS = [
   '/assets/js/vendor/twitter/web-intents.js',
   '/assets/images/bg10-min.png',
   '/assets/images/fav-icons/favicon-32x32.png',
+  '/assets/images/pages/donald-boulton-100.jpg',
+  '/assets/images/pages/reviews.jpg',
+  '/assets/images/pages/digital_box_1400-compressor.jpg',
+  '/assets/images/pages/stop-spam.jpg',
+  '/assets/images/pages/jekyll-reversed.jpg',
+  '/assets/photography/cat-gallery/einstine-1200.jpg',
+  '/assets/images/pages/performance/speedtest_donboulton-1200.jpg',
+  '/assets/images/pages/staticman-github-gitlab.png',
+  '/assets/images/pages/jekyll-admin-1200.jpg',
+  '/assets/images/pages/windows-tweaks-1200.png',
+  '/assets/images/pages/defrag-sql-2014-1200.png',
+  '/assets/images/pages/letter-avatar-header-1200.png',
+  '/assets/images/pages/social-sharing.jpg',
+  '/assets/images/pages/defrag-sql-2014-1200.png',
+  '/assets/images/pages/check_sql_fragmentation-1200.png'
+  '/assets/images/pages/stop-spam-1200.jpg',
+  '/assets/images/pages/reviews-1200.jpg',
+  '/assets/images/pages/social-sharing-1200.jpg',
+  '/assets/images/pages/jekyll-reversed-1200.jpg',
+  '/assets/images/pages/stop-spam-320.jpg',
+  '/assets/images/pages/jekyll-admin-320.jpg',
+  '/assets/images/pages/windows-tweaks-320.png',
+  '/assets/images/pages/defrag-sql-2014-320.png',
+  '/assets/images/pages/letter-avatar-header-320.png',
+  '/assets/images/pages/social-sharing-320.jpg',
+  '/assets/images/pages/defrag-sql-2014-320.png',
+  '/assets/images/pages/check_sql_fragmentation-320.png',
+  '/assets/images/pages/reviews-320.jpg',
+  '/assets/images/pages/social-sharing-320.jpg',
+  '/assets/images/pages/jekyll-reversed-320.jpg',
+  '/assets/icons/icons.svg',
+  '/assets/images/pages/github-74-80.png',
+  '/assets/images/pages/cf-logo-v-rgb-rev-239-80.png',
+  ''
   '/sw.js',
   '/manifest.webmanifest',
 ];
---
+++ workers/push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1532977531.56
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://donboulton.com/assets/images/fav\u002Dicons/favicon.png',
+  defaultTitle: 'Donald Boulton',
+  defaultActionURL: 'https://donboulton.com',
+  appKey: '1aQDr7-6SVyBngQwRKgXtA',
+  token: 'MToxYVFEcjctNlNWeUJuZ1F3UktnWHRBOkxGMmM1UmJyN2h0c2NLRmVZUHpTN2VRNThYUlFQTnk1TjFTT3N1OFBVOFk',
+  vapidPublicKey: 'BJHTWH3_SATKp4Exa2oaFNYk92PSXIQl-wODOTPU300bMbwCP8QiQkKG82LYw0PQlz0bCC73HZeWe6q88apmQFQ='
+})
