8f5a3466f166559aa8d30b974a4da7840c568b71
Donald W. Boulton <donaldboulton@gmail.com>
2018-08-08 20:11:39 -0500

    DWB

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = '22';
+const VERSION = '20';
 
 this.addEventListener('install', (e) => {
     e.waitUntil(caches.open(VERSION).then(cache => cache.addAll([
@@ -411,6 +411,8 @@ self.addEventListener('message', (event) => {
     }
 });
 
+var messaging = firebase.messaging();
+
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
@@ -419,7 +421,6 @@ firebase.initializeApp({
   'messagingSenderId': '857761645811'
 });
 
-var messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
  console.log('[sevice-worker.js] Received background message ', payload);
 
