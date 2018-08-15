0d1e4588ced12265a5f7b33c12f6d7ab896fb0bd
Donald Boulton <donaldboulton@gmail.com>
2018-08-14 20:12:24 -0500

    get it

--- assets/js/vendor/notifications/notifications.js
+++ assets/js/vendor/notifications/notifications.js
@@ -20,19 +20,21 @@
     });
   };
     const allOptionsNotification = function(registration) {
-    const title = 'Web Push Book';
+    const title = 'donboulton.com Notify';
     const options = {
-      body: 'This would be the body text of the notification.\n' +
-        'It can hold two lines of text.',
+      body: 'Comment Updates Notifications.\n' +
+        'There has been a new comment on donboulton.com.',
       icon: '/assets/images/fav-icons/favicon-512x512.png',
       badge: '/assets/images/fav-icons/favicon-192x192.png',
       image: '/assets/images/pages/letter-avatar-header.png',
+      sound: '/assets/audio/notification-sound.mp3',
       tag: 'notification',
+      vibrate: [500,110,500,110,450,110,200,110,170,40,450,110,200,110,170,40,500],
       actions: [
         {
           action: 'download-book-action',
           title: 'Download Book',
-          icon: '/assets/images/fav-icons/favicon-128x128.png'
+          icon: '/assets/icons/download.svg'
         }
       ]
     };
@@ -71,7 +73,7 @@
     /**** START iconNotification ****/
     const title = 'Icon Notification';
     const options = {
-      icon: '/assets/images/fav-icons/favicon-512x512.png'
+      icon: '/assets/images/fav-icons/favicon-192x192.png'
     };
     registration.showNotification(title, options);
     /**** END iconNotification ****/
@@ -91,7 +93,7 @@
     /**** START imageNotification ****/
     const title = 'Image Notification';
     const options = {
-      image: '/assets/images/pages/letter-avatar-header.png'
+      image: '/assets/images/fav-icons/favicon-192x192.png'
     };
     registration.showNotification(title, options);
     /**** END imageNotification ****/
@@ -174,22 +176,22 @@
         {
           action: 'coffee-action',
           title: 'Coffee',
-          icon: '/assets/images/action-1-128x128.png'
+          icon: '/assets/images/fav-icons/favicon-192x192.png'
         },
         {
           action: 'doughnut-action',
           title: 'Doughnut',
-          icon: '/assets/images/action-2-128x128.png'
+          icon: '/assets/images/fav-icons/favicon-192x192.png'
         },
         {
           action: 'gramophone-action',
           title: 'gramophone',
-          icon: '/assets/images/action-3-128x128.png'
+          icon: '/images/demos/action-3-128x128.png'
         },
         {
           action: 'atom-action',
           title: 'Atom',
-          icon: '/assets/images/action-4-128x128.png'
+          icon: '/assets/images/fav-icons/favicon-192x192.png'
         }
       ]
     };
@@ -379,7 +381,7 @@
     return promiseTimeout(() => {
       const serviceWorker = registration.install || registration.waiting ||
         registration.active;
-      serviceWorker.postMessage('must-show-notification-demo');
+      serviceWorker.postMessage('must-show-notification');
     }, 4000);
   };
 
@@ -387,7 +389,7 @@
     return promiseTimeout(() => {
       const serviceWorker = registration.install || registration.waiting ||
         registration.active;
-      serviceWorker.postMessage('send-message-to-page-demo');
+      serviceWorker.postMessage('send-message-to-page');
     }, 4000);
   };
 
@@ -570,6 +572,28 @@
       }
     ];
 
+    return registerServiceWorker()
+    .then(function(registration) {
+      configs.forEach(function(config) {
+        const button = document.querySelector(`.${config.className}`);
+        if (!button) {
+          console.error('No button found with classname: ', config.className);
+          return;
+        }
+        button.addEventListener('click', function() {
+          const promiseResult = config.cb(registration);
+          if (promiseResult) {
+            button.disabled = true;
+            promiseResult.then(() => {
+              button.disabled = false;
+            })
+          }
+        });
+        button.disabled = !config.enabled();
+      });
+    });
+  };
+
   window.addEventListener('load', function() {
     if (!('serviceWorker' in navigator)) {
       // Service Worker isn't supported on this browser, disable or hide UI.
@@ -597,5 +621,5 @@
       }
     });
   });
-};
-!function(e){function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}var r=window.webpackJsonp;window.webpackJsonp=function(t,a,i){for(var u,c,f,s=0,l=[];s<t.length;s++)c=t[s],o[c]&&l.push(o[c][0]),o[c]=0;for(u in a)Object.prototype.hasOwnProperty.call(a,u)&&(e[u]=a[u]);for(r&&r(t,a,i);l.length;)l.shift()();if(i)for(s=0;s<i.length;s++)f=n(n.s=i[s]);return f};var t={},o={2:0};n.e=function(e){function r(){u.onerror=u.onload=null,clearTimeout(c);var n=o[e];0!==n&&(n&&n[1](new Error("Loading chunk "+e+" failed.")),o[e]=void 0)}var t=o[e];if(0===t)return new Promise(function(e){e()});if(t)return t[2];var a=new Promise(function(n,r){t=o[e]=[n,r]});t[2]=a;var i=document.getElementsByTagName("head")[0],u=document.createElement("script");u.type="text/javascript",u.charset="utf-8",u.async=!0,u.timeout=12e4,n.nc&&u.setAttribute("nonce",n.nc),u.src=n.p+"js/"+({0:"main",1:"vendor"}[e]||e)+"."+{0:"95ae1",1:"31b1e"}[e]+".js";var c=setTimeout(r,12e4);return u.onerror=u.onload=r,i.appendChild(u),a},n.m=e,n.c=t,n.d=function(e,r,t){n.o(e,r)||Object.defineProperty(e,r,{configurable:!1,enumerable:!0,get:t})},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},n.p="",n.oe=function(e){throw e}}([]);
+})();
+
