fbcbdc4659e164808bbf8fc9ec0fcb524f29a880
Donald Boulton <donaldboulton@gmail.com>
2018-08-22 07:04:26 -0500

    netlify cms intigration

---
+++ assets/js/vendor/admin/login.js
@@ -0,0 +1,15 @@
+const netlifyIdentity = require("netlify-identity-widget");
+
+componentDidMount(){
+  netlifyIdentity.init();
+}
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
