d6eeac71c967e7114d1ee130620f7f1cae9cf761
Donald Boulton <donaldboulton@gmail.com>
2018-08-23 12:31:24 -0500

    div root and widget init in app.js

--- app.js
+++ app.js
@@ -205,6 +205,34 @@ window.onload = function() {
   setUpPush();
 };
 
+const netlifyIdentity = require("netlify-identity-widget");
+
+netlifyIdentity.init({
+  container: "#netlify-modal" // defaults to document.body,
+});
+
+netlifyIdentity.open(); // open the modal
+netlifyIdentity.open("login"); // open the modal to the login tab
+netlifyIdentity.open("signup"); // open the modal to the signup tab
+
+netlifyIdentity.on("init", user => console.log(user));
+netlifyIdentity.on("login", user => console.log(user));
+netlifyIdentity.on("logout", () => console.log("Logged out"));
+netlifyIdentity.on("error", err => console.error("Logged out"));
+netlifyIdentity.on("open", () => console.log("Widget opened"));
+netlifyIdentity.on("close", () => console.log("Widget closed"));
+
+// Close the modal
+netlifyIdentity.close();
+
+// Log out the user
+netlifyIdentity.logout();
+
+// Access the underlying GoTrue JS client.
+// Note that doing things directly through the GoTrue client brings a risk of getting out of
+// sync between your state and the widget’s state.
+netlifyIdentity.gotrue;
+
 const hash          = document.location.hash;
 const currentUrlEl  = document.getElementById('current-url');
 const clientIdForm  = document.getElementById('client-id-form');
