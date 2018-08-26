e262db1ebf0ccdc60bdad93f38aea77091a1389e
Donald Boulton <donaldboulton@gmail.com>
2018-08-26 17:03:01 -0500

    admin work push fixed

--- assets/js/vendor/notifications/admin.js
+++ assets/js/vendor/notifications/admin.js
@@ -85,3 +85,9 @@ window.addEventListener('load', () => {
     }
   });
 });
+
+var secondUpload = new FileUploadWithPreview('mySecondImage', true)
+var secondUploadInfoButton = document.querySelector('.upload-info-button--second');
+secondUploadInfoButton.addEventListener('click', function(){
+    console.log('Second upload:', secondUpload, secondUpload.cachedFileArray)
+});
