6b7a8b7b053717289f5eebea5993ab9ee0d9558c
Donald Boulton <donaldboulton@gmail.com>
2018-08-29 12:59:58 -0500

    path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
       // we're going to put the generated file in the assets folder so jekyll will grab it.
-        path: 'src/assets/javascripts/',
+        path: '/src/assets/javascripts/',
         filename: "bundle.js"
     },
     module: {
