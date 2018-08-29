fac2890d2cf91bc8085d8a72e47e2c7b4d6f8e65
Donald Boulton <donaldboulton@gmail.com>
2018-08-29 11:11:52 -0500

    folder cage

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
       // we're going to put the generated file in the assets folder so jekyll will grab it.
-        path: 'src/assets/js/',
+        path: './assets/js/',
         filename: "bundle.js"
     },
     module: {
