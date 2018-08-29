679289911c3e6abae54c81bb29dc35a46f4d4a3f
Donald Boulton <donaldboulton@gmail.com>
2018-08-29 11:03:50 -0500

    package.json webpack --watch

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
     // webpack folder's entry js - excluded from jekll's build process.
-    entry: "./webpack/app.js",
+    entry: {
+        app: './app.js',
+        main: './webpack/entry.js',
+    },
     output: {
       // we're going to put the generated file in the assets folder so jekyll will grab it.
         path: 'src/assets/js/',
