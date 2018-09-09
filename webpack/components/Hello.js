c15c2df18d6bcf57a6c8765d116b192bd3382bc8
Donald Boulton <donaldboulton@gmail.com>
2018-09-09 09:46:20 -0500

    jsdom error

--- webpack/components/Hello.js
+++ webpack/components/Hello.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+
+import * as RealtimeMessaging from 'realtime-messaging';
+
+const client = RealtimeMessaging.createClient();
+client.setClusterUrl("http://ortc-developers.realtime.co/server/2.1/");
+client.connect('zeTkBw', 'token');
+
+client.onConnected = (client) => {
+    console.log("realtime connected");
+    client.subscribe("channel", true, (client, channel, message) => {
+        console.log("Received message:", message);
+    });
+}
+
 class Hello extends Component {
   render() {
     return (
