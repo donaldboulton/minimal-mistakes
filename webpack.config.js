4bda62a0bc68d46ffef26e709377be0f27bc3bb8
Donald Boulton <donaldboulton@gmail.com>
2018-09-09 06:38:37 -0500

    ship

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    chunkFilename: 'chunck.[name].[id].[hash].js',
+    chunkFilename: '[name].[id].js',
   },
   mode: 'production',
   devtool: 'inline-source-map',
