4650780fa105dc21f5b7f84e0d7fd565f1cd59a6
Donald Boulton <donaldboulton@gmail.com>
2018-09-16 20:42:12 -0500

    jquery issues

---
+++ _src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { initializeFirebase } from './push-notification';
+ReactDOM.render(<App />, document.getElementById('root'));
+initializeFirebase();
