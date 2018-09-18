a506f0908f1ae734379efa86a3021ab386b9229e
Donald Boulton <donaldboulton@gmail.com>
2018-09-18 16:36:33 -0500

    filx gallery mports

--- _src/Gallery.js
+++ _src/Gallery.js
@@ -1,33 +1,24 @@
-Promise.all([
-  import('lightgallery'),
-  import('lg-thumbnail.js'),
-  import('lg-autoplay.js'),
-  import('lg-fullscreen.js'),
-  import('lg-zoom.js'),
-  import('lg-share.js'),
-  import('lg-pager.js')
-])
-.then(([]) => {
+require('lightgallery.js');
+require('lg-thumbnail.js');
+require('lg-autoplay.js');
+require('lg-zoom.js');
+require('lg-fullscreen.js');
+require('lg-pager.js');
+require('lg-autoplay.js');
   
-  window.prettyPrint && prettyPrint()
+window.prettyPrint && prettyPrint()
 
-    var $gallery = $("#aniimated-thumbnials").lightGallery({
-            thumbnail: true,
-            selector: '.image'
-          });
-          var $grid = $('#aniimated-thumbnials').isotope({
-            percentPosition: true,
-            columnWidth: '#gallery-sizer',
-            itemSelector: '.image-wrapper',
-            layoutMode: "masonry"
-          });
+var $gallery = $("#aniimated-thumbnials").lightGallery({
+    thumbnail: true,
+    selector: '.image'
+});
+var $grid = $('#aniimated-thumbnials').isotope({
+    percentPosition: true,
+    columnWidth: '#gallery-sizer',
+    itemSelector: '.image-wrapper',
+    layoutMode: "masonry"
+});
           
-          // layout Isotope after each image loads
-          $grid.imagesLoaded().progress( function() {
-            $grid.masonry();
-          });          
-        })
-        .catch(error => {
-           console.log(error);
-           toastr.error('An error occurred while loading the lightgallery module', "Module Load Failed");
+$grid.imagesLoaded().progress( function() {
+    $grid.masonry();    
 });
