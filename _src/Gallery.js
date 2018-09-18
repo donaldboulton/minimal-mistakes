db5aa1ea1af1cdc26198523658ecbc05d8be482c
Donald Boulton <donaldboulton@gmail.com>
2018-09-18 16:49:28 -0500

    light gallery all

--- _src/Gallery.js
+++ _src/Gallery.js
@@ -1,24 +1,20 @@
-require('lightgallery.js');
-require('lg-thumbnail.js');
-require('lg-autoplay.js');
-require('lg-zoom.js');
-require('lg-fullscreen.js');
-require('lg-pager.js');
-require('lg-autoplay.js');
-  
-window.prettyPrint && prettyPrint()
+require(['./lightgallery-all.js'], function() {
+  require([], function() {
+    window.prettyPrint && prettyPrint()
 
-var $gallery = $("#aniimated-thumbnials").lightGallery({
-    thumbnail: true,
-    selector: '.image'
-});
-var $grid = $('#aniimated-thumbnials').isotope({
-    percentPosition: true,
-    columnWidth: '#gallery-sizer',
-    itemSelector: '.image-wrapper',
-    layoutMode: "masonry"
-});
+    var $gallery = $("#aniimated-thumbnials").lightGallery({
+      thumbnail: true,
+      selector: '.image'
+    });
+    var $grid = $('#aniimated-thumbnials').isotope({
+      percentPosition: true,
+      columnWidth: '#gallery-sizer',
+      itemSelector: '.image-wrapper',
+      layoutMode: "masonry"
+    });
           
-$grid.imagesLoaded().progress( function() {
-    $grid.masonry();    
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry();    
+    });
+  });
 });
