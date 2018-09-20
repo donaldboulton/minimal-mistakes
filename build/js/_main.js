1397b0d5f39fc362f9d1698d914c0854ba47a917
Donald Boulton <donaldboulton@gmail.com>
2018-09-20 15:21:23 -0500

    build sw from webpack config

--- build/js/_main.js
+++ build/js/_main.js
@@ -53,6 +53,26 @@ $(document).ready(function() {
     }, 400);
   });
 });
+$(window).bind("hashchange", function(event) {
+  $.smoothScroll({
+    // Replace '#/' with '#' to go to the correct target
+    scrollTarget: location.hash.replace(/^\#\/?/, "#"),
+    offset: -20
+  });
+});
+$('a[href*="#"]').bind("click", function(event) {
+  // Remove '#' from the hash.
+  var hash = this.hash.replace(/^#/, "");
+  if (this.pathname === location.pathname && hash) {
+    event.preventDefault();
+    // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
+    location.hash = "#/" + hash;
+  }
+});
+// Trigger hashchange event on page load if there is a hash in the URL.
+if (location.hash) {
+  $(window).trigger("hashchange");
+}
 $('.gallery').each( function() {
   var $pic     = $(this),
       getItems = function() {
