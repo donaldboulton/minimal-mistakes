a533d627f8b23dd2225a7db9421217726f9860e6
Donald Boulton <donaldboulton@gmail.com>
2018-10-06 06:54:30 -0500

    init site no gallery

--- build/js/init.site.js
+++ build/js/init.site.js
@@ -53,26 +53,3 @@ $(document).ready(function() {
     }, 400);
   });
 });
-$('.gallery').each( function() {
-  var $pic     = $(this),
-      getItems = function() {
-          var items = [];
-          $pic.find('a').each(function() {
-              var $href   = $(this).attr('href'),
-                  $size   = $(this).data('size').split('x'),
-                  $width  = $size[0],
-                  $height = $size[1];
-
-              var item = {
-                  src : $href,
-                  w   : $width,
-                  h   : $height
-              };
-
-              items.push(item);
-          });
-          return items;
-      };
-
-  var items = getItems();
-});
