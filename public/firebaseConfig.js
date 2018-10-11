89e1da9969908053cf1c55a19d0b10330743c9e0
Donald Boulton <donaldboulton@gmail.com>
2018-10-11 08:11:54 -0500

    more firbase nonsense

--- public/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: 'AIzaSyBoZgIki3tEgCtgSVVWDdastZCqW9WWGKE',
-  authDomain: 'airy-office-413.firebaseapp.com',
-  databaseURL: 'https://airy-office-413.firebaseio.com',
-  projectId: 'airy-office-413',
-  storageBucket: 'airy-office-413.appspot.com',
-  messagingSenderId: '857761645811',
-};
