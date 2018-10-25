c92ef90cd90196b9b9482522fb80f4bfc8d6e766
Donald Boulton <donaldboulton@gmail.com>
2018-10-24 20:17:59 -0500

    icon color

--- _src/lib/components/AppNotify.js
+++ _src/lib/components/AppNotify.js
@@ -86,7 +86,7 @@ class App extends React.Component {
 
     return (
       <div className="Appcenter">
-        <h3 className="Appheader"><img src={bell} className="icon icon--bell" alt="Notify" /> React Notify </h3>
+        <h3 className="Appheader"><img src={bell} className="icon icon--bell fa-bell" alt="Notify" /> React Notify </h3>
         <div className='notifyCenter'>
         <button className="btn btn--primary" onClick={this.handleButtonClick.bind(this)}>Notif!</button>
         <Notification
