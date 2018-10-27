d1717c03af9e6d3b93e0720112ed89e4f6c3b9e2
Donald Boulton <donaldboulton@gmail.com>
2018-10-27 11:50:06 -0500

    react in scope

--- _src/lib/components/ReactChatSlack.js
+++ _src/lib/components/ReactChatSlack.js
@@ -1,10 +1,11 @@
+import React, { Component}  from 'react';
 import ReactChatSlack from 'react-chat-slack';
  
 class App extends Component {
   render() {
     return (
       <ReactChatSlack
-        token="xoxb-465463990983-466950233063-2Cw0IV1XmiNBiacPnb7RQKUE"
+        token="eG94Yi00NjU0NjM5OTA5ODMtNDY2OTUwMjMzMDYzLTJDdzBJVjFYbWlOQmlhY1BuYjdSUUtVRQ=="
         channel_id="D8PMJ6MQC"
         username="donboulton"
         title="Need Help"
