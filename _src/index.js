a3e1491b18fd9641690798d1734ee1004408c18d
Donald Boulton <donaldboulton@gmail.com>
2018-10-31 12:36:22 -0500

    dropdown menu

--- _src/index.js
+++ _src/index.js
@@ -2,71 +2,12 @@ import React from 'react';
 import './lib/components/css/Rapp.css';
 import Todo from './lib/components/Todo';
 import AppNotify from './lib/components/AppNotify';
+import Dropdown from './lib/components/dropdown/dropdown';
 
 var $ = require('jquery');
 window.jQuery = $;
 window.$ = $;
-
-class Card extends React.Component {
-  constructor() {
-    super();
-    
-    this.state = {
-      showMenu: false,
-    };
-    
-    this.showMenu = this.showMenu.bind(this);
-    this.closeMenu = this.closeMenu.bind(this);
-  }
   
-  showMenu(event) {
-    event.preventDefault();
-    
-    this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu);
-    });
-  }
-  
-  closeMenu(event) {
-    
-    if (!this.dropdownMenu.contains(event.target)) {
-      
-      this.setState({ showMenu: false }, () => {
-        document.removeEventListener('click', this.closeMenu);
-      });  
-      
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.showMenu}>
-          Show menu
-        </button>
-        
-        {
-          this.state.showMenu
-            ? (
-              <div
-                className="menu"
-                ref={(element) => {
-                  this.dropdownMenu = element;
-                }}
-              >
-                <button> Menu item 1 </button>
-                <button> Menu item 2 </button>
-                <button> Menu item 3 </button>
-              </div>
-            )
-            : (
-              null
-            )
-        }
-      </div>
-    );
-  }
-}
 class Title extends React.Component{
   render(){
     return(
@@ -95,11 +36,18 @@ class Layout extends React.Component {
         {/* <Header /> */}
         <Todo/>
         <AppNotify/>
-        <Card/>
         {/* <Footer/> */}
       </div>
     );
   }
 }
+
+var displayDropdown = (
+  <div style={{display: 'flex', justifyContent: 'center'}} >
+    <Dropdown />
+  </div>
+);
+
+ReactDOM.render(displayDropdown, document.getElementById('root'));
 
 import './main.css';
