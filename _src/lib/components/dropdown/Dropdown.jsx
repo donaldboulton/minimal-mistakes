4b3f7ffacd7b96447ca465a1f30e319bac9ae3a2
Donald Boulton <donaldboulton@gmail.com>
2018-11-01 10:02:21 -0500

    remove modal errors

--- _src/lib/components/dropdown/Dropdown.jsx
+++ _src/lib/components/dropdown/Dropdown.jsx
@@ -31,14 +31,12 @@ showDropdownMenu(event) {
   render() {
     return (
         <div className="dropdown" style = {{background:"#1d1d1d",width:"80px"}} >
-         <div className="bm-burger-button" style="z-index: 1000;" onClick={this.showDropdownMenu}>
-         <button className="button">
+         <div className="bm-burger-button button" style="z-index: 1000;" onClick={this.showDropdownMenu}>
             <span>
             <span className="bm-burger-bars" style="position: absolute; height: 20%; left: 0px; right: 0px; top: 0%; opacity: 1;"></span>
             <span className="bm-burger-bars" style="position: absolute; height: 20%; left: 0px; right: 0px; top: 40%; opacity: 1;"></span>
             <span className="bm-burger-bars" style="position: absolute; height: 20%; left: 0px; right: 0px; top: 80%; opacity: 1;"></span>
             </span>
-          </button>
          </div>
           { this.state.displayMenu ? (
           <ul className="toc__menu">
