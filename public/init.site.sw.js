27f6dc03e62ebd88f743afef9565527db338b08e
Donald Boulton <donaldboulton@gmail.com>
2018-11-03 09:58:52 -0500

    bundle

--- public/init.site.sw.js
+++ public/init.site.sw.js
@@ -195,4 +195,4 @@ if ('serviceWorker' in navigator) {
     console.log('Service worker successfully registered on scope', registration.scope);
   });
 }
-$(document).ready(function(){$("a").smoothScroll({offset:-20})});var coll=document.getElementsByClassName("collapsible");var i;for(i=0;i<coll.length;i++){coll[i].addEventListener("click",function(){this.classList.toggle("active");var content=this.nextElementSibling;if(content.style.maxHeight){content.style.maxHeight=null}else{content.style.maxHeight=content.scrollHeight+"px"}})}
+var coll=document.getElementsByClassName("collapsible");var i;for(i=0;i<coll.length;i++){coll[i].addEventListener("click",function(){this.classList.toggle("active");var content=this.nextElementSibling;if(content.style.maxHeight){content.style.maxHeight=null;}else{content.style.maxHeight=content.scrollHeight+"px"}})
