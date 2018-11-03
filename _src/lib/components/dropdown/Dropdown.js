01c7f37997ce9abe59ceb283b051b90b9f0c9305
Donald Boulton <donaldboulton@gmail.com>
2018-11-03 10:32:55 -0500

    menu work

--- _src/lib/components/dropdown/Dropdown.js
+++ _src/lib/components/dropdown/Dropdown.js
@@ -32,17 +32,30 @@ showDropdownMenu(event) {
   render() {
     return (
         <div  className="dropdown">
-	        <div className="button" onClick={this.showDropdownMenu}>
-            <span>
-              <span className="bm-burger-bars"></span>
-              <span className="bm-burger-bars"></span>
-              <span className="bm-burger-bars"></span>
-            </span>
+	        <div className="menu-trigger" onClick={this.showDropdownMenu}>
+          <span className="start">
+            <a href="#">
+                <svg preserveAspectRatio="xMidYMid meet" focusable="false" viewBox="-5 -5 34 34">
+                    <g><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="white"></path></g>
+                </svg>
+            </a>
+          </span>
           </div>
 
           { this.state.displayMenu ? (
-          <ul>
+          <ul className="menu" role="menu">
     		   <li><a className="active" href="/admin/index.html">Create Page</a></li>
+              <ul>
+                <li><a href="#">Level 3</a>
+                  <ul>
+                      <li><a href="#">Level 4</a></li>
+                      <li><a href="#">Level 4</a></li>
+                      <li><a href="#">Level 4</a></li>
+                  </ul>
+                  </li>
+                      <li><a href="#">Level 3</a></li>
+                      <li><a href="#">Level 3</a></li>
+                </ul>
     		   <li><a href="/">Home</a></li>
     		   <li><a href="/year-archive/">Posts</a></li>
     		   <li><a href="/about/">About</a></li>
