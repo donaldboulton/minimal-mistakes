f38325a892be54035105d4012c833e5ee26b001c
Donald Boulton <donaldboulton@gmail.com>
2018-11-04 19:44:20 -0600

    hand build popups

--- _src/lib/components/Popup.js
+++ _src/lib/components/Popup.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import Popup from 'react-popup';
+class Popup extends React.Component {
+  render() {
+    return (
+      <div className='popup'>
+        <div className='popup_inner'>
+          <h1>{this.props.text}</h1>
+        <button onClick={this.props.closePopup}>close me</button>
+        </div>
+      </div>
+    );
+  }
+}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showPopup: false
+    };
+  }
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+  render() {
+    return (
+      <div className='app'>
+        <h1>hihi</h1>
+        <button onClick={this.togglePopup.bind(this)}>show react popup</button>        
+        {this.state.showPopup ? 
+          <Popup
+            text='Close Me'
+            closePopup={this.togglePopup.bind(this)}
+          />
+          : null
+        }
+      </div>
+    );
+  }
+}
 
-ReactDom.render(
-    <Popup />,
-    document.getElementById('popupContainer')
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('popup')
 );
+
