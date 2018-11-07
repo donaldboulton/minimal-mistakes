4b35f2f62c47af83658597d228fd4241a99e3596
Donald Boulton <donaldboulton@gmail.com>
2018-11-07 16:32:19 -0600

    dialog more

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
@@ -34,7 +33,6 @@ module.exports = {
       from: path.resolve('_images'),
       to: 'images/',
     }]),
-    new BundleAnalyzerPlugin(),
     new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i })
   ],
   devServer: {
