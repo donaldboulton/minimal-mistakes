fe1f014bb3b22b315b6db3cabc2a951db472b1a8
Donald Boulton <donaldboulton@gmail.com>
2018-11-27 22:27:19 -0600

    webpack changed

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
@@ -38,7 +37,6 @@ module.exports = {
       to: 'images/',
     }]),
     new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
-    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     contentBase: './assets',
