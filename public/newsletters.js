d0a5209b708f264513cec1de8b1e874e205fa769
Donald Boulton <donaldboulton@gmail.com>
2018-12-02 12:45:12 -0600

    news change

--- public/newsletters.js
+++ public/newsletters.js
@@ -1,21 +1,21 @@
-var form = document.getElementById('newsletter')
+var form = document.getElementById('newsletter');
 form.addEventListener("submit", function(e) {
     // stop form from submitting
     e.preventDefault()
     var email = document.getElementById('inputEmail').value
-    submitEmail(email)
+    submitEmail(email);
 })
 function submitEmail(email) {
-  fetch('/.netlify/functions/form-handler', {
+  fetch('/.netlify/lambda-build/form-handler', {
       method: 'post',
       body: JSON.stringify({
         email: email
       })
     }).then(function(response) {
-      return response.json()
+      return response.json();
     }).then(function(data) {
       console.log('data from function', data)
       var messageDiv = document.getElementById('message')
-      messageDiv.innerText = 'Email added via Netlify functions & AJAX!'
-    })
+      messageDiv.innerText = 'Email added via Netlify functions & AJAX!';
+    });
 }
