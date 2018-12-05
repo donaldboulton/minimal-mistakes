3d43da672f66d408fa1d7c1065a85d817f605b1d
Donald Boulton <donaldboulton@gmail.com>
2018-12-05 10:58:53 -0600

    d

--- _src/index.js
+++ _src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Todo from './lib/components/Todo';
 import AppNotify from './lib/components/AppNotify';
 import Dialog from './lib/components/Dialog';
-class Layout extends React.Component {
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+class App extends React.Component {
   render() {
     return (
       <div>
