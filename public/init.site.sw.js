f6595fdff2c0a942de203ebaccc5e8730d764848
Donald Boulton <donaldboulton@gmail.com>
2018-12-06 08:56:53 -0600

    menu work collapsable

--- public/init.site.sw.js
+++ public/init.site.sw.js
@@ -128,19 +128,53 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 (function() {
-  const headings = document.querySelectorAll('h2')
-
+  // Get all the <h2> headings
+  const headings = document.querySelectorAll('#mmain h3')
+  
   Array.prototype.forEach.call(headings, heading => {
-    let btn = heading.querySelector('button')
-    let target = heading.nextElementSibling
+    heading.innerHTML = `
+      <button aria-expanded="false">
+        ${heading.textContent}
+        <svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+          <rect class="vert" height="8" width="2" y="1" x="4"/>
+          <rect height="2" width="8" y="4" x="1"/>
+        </svg>
+      </button>
+    `;
+    const getContent = (elem) => {
+      let elems = [];
+      while (elem.nextElementSibling && elem.nextElementSibling.tagName !== 'H3') {
+        elems.push(elem.nextElementSibling);
+        elem = elem.nextElementSibling;
+      }
+      
+      elems.forEach((node) => {
+        node.parentNode.removeChild(node);
+      })
 
+      return elems;
+    };
+    
+    let contents = getContent(heading);
+    
+    let wrapper = document.createElement('div');
+    wrapper.hidden = true;
+    
+    contents.forEach(node => {
+      wrapper.appendChild(node);
+    });
+    
+    heading.parentNode.insertBefore(wrapper, heading.nextElementSibling);
+    
+    let btn = heading.querySelector('button');
+    
     btn.onclick = () => {
       let expanded = btn.getAttribute('aria-expanded') === 'true' || false;
-
-      btn.setAttribute('aria-expanded', !expanded)
-      target.hidden = expanded;
+      
+      btn.setAttribute('aria-expanded', !expanded);
+      wrapper.hidden = expanded;
     }
-  })
+  });
 })();
 var dialog = document.getElementById('my-accessible-dialog');
 
