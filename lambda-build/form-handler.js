55f932ffd6863b24bbdee2e5adb9207f6f2fff6e
Donald Boulton <donaldboulton@gmail.com>
2018-12-07 17:44:33 -0600

    lambda build

---
+++ lambda-build/form-handler.js
@@ -0,0 +1,128 @@
+(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 210);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 210:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _querystring = __webpack_require__(24);
+
+exports.handler = (event, context, callback) => {
+  let body = {};
+  console.log(event);
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    body = (0, _querystring.parse)(event.body);
+  }
+
+  // Bail if email is missing
+  if (!body.email) {
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'missing email'
+      })
+    });
+  }
+
+  // Do my email subscription logic
+
+
+  if (event.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    // Do redirect for non JS enabled browsers
+    return callback(null, {
+      statusCode: 302,
+      headers: {
+        Location: '/thanks.html',
+        'Cache-Control': 'no-cache'
+      },
+      body: JSON.stringify({})
+    });
+  }
+
+  // Return data to AJAX request
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      emailAdded: true
+    })
+  });
+};
+
+/***/ }),
+
+/***/ 24:
+/***/ (function(module, exports) {
+
+module.exports = require("querystring");
+
+/***/ })
+
+/******/ })));
