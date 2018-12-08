8b6c06f1145ee70ae8228be2a022b2fb43565dea
Donald Boulton <donaldboulton@gmail.com>
2018-12-07 18:29:18 -0600

    test build

--- lambda-build/form-handler.js
+++ lambda-build/form-handler.js
@@ -60,18 +60,18 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 210);
+/******/ 	return __webpack_require__(__webpack_require__.s = 199);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 210:
+/***/ 199:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _querystring = __webpack_require__(24);
+var _querystring = __webpack_require__(22);
 
 exports.handler = (event, context, callback) => {
   let body = {};
@@ -118,7 +118,7 @@ exports.handler = (event, context, callback) => {
 
 /***/ }),
 
-/***/ 24:
+/***/ 22:
 /***/ (function(module, exports) {
 
 module.exports = require("querystring");
