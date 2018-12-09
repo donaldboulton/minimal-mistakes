eb9efba6d8247cad283ddbee2eab2d89c3272eba
Donald Boulton <donaldboulton@gmail.com>
2018-12-09 07:11:28 -0600

    config env

--- _src/lib/components/Dialog.jsx
+++ _src/lib/components/Dialog.jsx
@@ -13,7 +13,7 @@ class DialogMenu extends React.Component {
     return (
       <div>
         <button id="myBtn" className="link-like" type="button" onClick={this.handleClick}>
-          <img src={bars} className="icon" alt="Menu" />
+          <img src={bars} className="icon"  alt="Menu" />
         </button>
 
         <Dialog id="my-accessible-dialog"
@@ -24,12 +24,12 @@ class DialogMenu extends React.Component {
           <p id="dialogDescription">
           <div id="mmain" className="gallery-4-col-unjustified" itemsScope="itemscope" itemType="https://schema.org/SiteNavigationElement">
             <div>
-    		        <span><h2 className="page-title"><a rel="noreferrer" itemProp="url" href="/">Pages</a></h2></span>
+    		        <span className="page-title"><a rel="noreferrer" itemProp="url" href="/">Pages</a></span>
                 <div className="toc1-menu-popup">
                    <div><a rel="noreferrer" itemProp="url" href="/">Home</a></div>
                    <div><a rel="noreferrer" itemProp="url" href="/about/">About</a></div>
                    <div><a rel="noreferrer" itemProp="url" href="/contact/">Contact</a></div>
-                   <h3><span><a rel="noreferrer" itemProp="url" href="/site-pages/">Site Pages&nbsp;&rarr;</a></span></h3>
+                   <span className="page-title"><a rel="noreferrer" itemProp="url" href="/site-pages/">Site Pages&nbsp;&rarr;</a></span>
                 </div>
             </div>
             <div>
@@ -38,7 +38,7 @@ class DialogMenu extends React.Component {
                   <div><a href="/programing/letter-avatar/">&nbsp;Letter Avatar</a></div>
                   <div><a href="/2017-04-11-post-social-sharing.html">&nbsp;Social Sharing</a></div>
                   <div><a href="/2018-05-27-post-reviews-for-jekyll.html">&nbsp;Jekyll Reviews</a></div>
-                  <h3><span><a rel="noreferrer" itemProp="url" href="/categories/">Post Archives&nbsp;&rarr;</a></span></h3>                     
+                  <span className="page-title"><a rel="noreferrer" itemProp="url" href="/categories/">Post Archives&nbsp;&rarr;</a></span>
                 </div>
             </div>
             <div>
@@ -47,7 +47,7 @@ class DialogMenu extends React.Component {
                   <div><a href="/photography/cat-gallery/">My Cats</a></div>
                   <div><a href="/photography/stray-cat-gallery/">Stray Cats</a></div>
                   <div><a href="/photography/old-cat-gallery/">Old Cats</a></div>
-                  <h3><span><a rel="noreferrer" itemProp="url" href="/photography/">All Galleries&nbsp;&rarr;</a></span></h3>
+                  <span className="page-title"><a rel="noreferrer" itemProp="url" href="/photography/">All Galleries&nbsp;&rarr;</a></span>
                 </div>
             </div>
             <div>
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -53,8 +53,20 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['react', 'stage-2', 'env'],
-            plugins: ["transform-class-properties"]
+            presets: [
+              "react",
+              [
+                  "env",
+                  {
+                      "targets": {
+                          "browsers": ["last 2 versions"]
+                      },
+                      "debug": true,
+                      "modules": "commonjs"
+                  }
+              ]
+          ],
+          plugins: ["transform-object-rest-spread", "transform-class-properties"]
           },
         },
       },
