342cea50f3c8ed10cdc902dfcea99007965b991f
Donald Boulton <donaldboulton@gmail.com>
2018-12-08 19:33:49 -0600

    page-title

--- _src/lib/components/Dialog.jsx
+++ _src/lib/components/Dialog.jsx
@@ -24,8 +24,8 @@ class DialogMenu extends React.Component {
           <p id="dialogDescription">
           <div id="mmain" className="gallery-4-col-unjustified" itemsScope="itemscope" itemType="https://schema.org/SiteNavigationElement">
             <div>
-    		        <span><h2 className="page__title"><a rel="noreferrer" itemProp="url" href="/">Pages</a></h2></span>
-                <div className="toc1__menu-popup">
+    		        <span><h2 className="page-title"><a rel="noreferrer" itemProp="url" href="/">Pages</a></h2></span>
+                <div className="toc1-menu-popup">
                    <div><a rel="noreferrer" itemProp="url" href="/">Home</a></div>
                    <div><a rel="noreferrer" itemProp="url" href="/about/">About</a></div>
                    <div><a rel="noreferrer" itemProp="url" href="/contact/">Contact</a></div>
@@ -33,8 +33,8 @@ class DialogMenu extends React.Component {
                 </div>
             </div>
             <div>
-               <span className="page__title"><a rel="noreferrer" itemProp="url" href="/year-archive/">Posts</a></span>
-                <div className="toc1__menu-popup">
+               <span className="page-title"><a rel="noreferrer" itemProp="url" href="/year-archive/">Posts</a></span>
+                <div className="toc1-menu-popup">
                   <div><a href="/programing/letter-avatar/">&nbsp;Letter Avatar</a></div>
                   <div><a href="/2017-04-11-post-social-sharing.html">&nbsp;Social Sharing</a></div>
                   <div><a href="/2018-05-27-post-reviews-for-jekyll.html">&nbsp;Jekyll Reviews</a></div>
@@ -42,8 +42,8 @@ class DialogMenu extends React.Component {
                 </div>
             </div>
             <div>
-                <span className="page__title"><a rel="noreferrer" itemProp="url" href="/photography/">Galleries</a></span>
-                <div className="toc1__menu-popup">
+                <span className="page-title"><a rel="noreferrer" itemProp="url" href="/photography/">Galleries</a></span>
+                <div className="toc1-menu-popup">
                   <div><a href="/photography/cat-gallery/">My Cats</a></div>
                   <div><a href="/photography/stray-cat-gallery/">Stray Cats</a></div>
                   <div><a href="/photography/old-cat-gallery/">Old Cats</a></div>
@@ -51,8 +51,8 @@ class DialogMenu extends React.Component {
                 </div>
             </div>
             <div>
-                <span className="page__title"><a rel="noreferrer" itemProp="url" href="/admin/index.html">Admin</a></span>
-                <div className="toc1__menu-popup list-unstyled">
+                <span className="page-title"><a rel="noreferrer" itemProp="url" href="/admin/index.html">Admin</a></span>
+                <div className="toc1-menu-popup list-unstyled">
                    <div><a rel="noreferrer" itemProp="url" href="/admin/index.html">Admin Login</a></div>
     		           <div><a rel="noreferrer" itemProp="url" href="/admin/admin/">Edit</a></div>              
                 </div>
