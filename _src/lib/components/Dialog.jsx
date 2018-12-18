537e2f9e2d80941fcf4bf05fb75b8d5513a04c91
Donald Boulton <donaldboulton@gmail.com>
2018-12-18 07:50:39 -0600

    clean menu font changes

--- _src/lib/components/Dialog.jsx
+++ _src/lib/components/Dialog.jsx
@@ -24,37 +24,36 @@ class MyComponent extends React.Component {
           <p id="dialogDescription">
           <div id="mmain" className="gallery-4-col-unjustified" itemsScope="itemscope" itemType="https://schema.org/SiteNavigationElement">
             <div>
-    		        <span className="page-title"><a rel="noreferrer" itemProp="url" href="/">Pages</a></span>
-                <div className="toc1-menu-popup">
+    		        <span className="page__title"><a rel="noreferrer" itemProp="url" href="/">Pages</a></span>
+                <div className="toc1-menu">
                    <div><a rel="noreferrer" itemProp="url" href="/">Home</a></div>
                    <div><a rel="noreferrer" itemProp="url" href="/about/">About</a></div>
                    <div><a rel="noreferrer" itemProp="url" href="/contact/">Contact</a></div>
-                   <span className="page-title"><a rel="noreferrer" itemProp="url" href="/site-pages/">Site Pages&nbsp;&rarr;</a></span>
+                   <span className="page__title"><a rel="noreferrer" itemProp="url" href="/site-pages/">Site Pages&nbsp;&rarr;</a></span>
                 </div>
             </div>
             <div>
-               <span className="page-title"><a rel="noreferrer" itemProp="url" href="/year-archive/">Posts</a></span>
-                <div className="toc1-menu-popup">
+               <span className="page__title"><a rel="noreferrer" itemProp="url" href="/year-archive/">Posts</a></span>
+                <div className="toc1-menu">
                   <div><a rel="noreferrer" itemProp="url" href="/2018-02-22-post-Letter-Avatar.html">&nbsp;Letter Avatar</a></div>
                   <div><a rel="noreferrer" itemProp="url" href="/2017-04-11-post-social-sharing.html">&nbsp;Social Sharing</a></div>
                   <div><a rel="noreferrer" itemProp="url" href="/2018-05-27-post-reviews-for-jekyll.html">&nbsp;Jekyll Reviews</a></div>
-                  <span className="page-title"><a rel="noreferrer" itemProp="url" href="/categories/">Post Archives&nbsp;&rarr;</a></span>
+                  <span className="page__title"><a rel="noreferrer" itemProp="url" href="/categories/">Post Archives&nbsp;&rarr;</a></span>
                 </div>
             </div>
             <div>
-                <span className="page-title"><a rel="noreferrer" itemProp="url" href="/photography/">Galleries</a></span>
-                <div className="toc1-menu-popup">
+                <span className="page__title"><a rel="noreferrer" itemProp="url" href="/photography/">Galleries</a></span>
+                <div className="toc1-menu">
                   <div><a rel="noreferrer" itemProp="url" href="/photography/cat-gallery/">My Cats</a></div>
                   <div><a rel="noreferrer" itemProp="url" href="/photography/stray-cat-gallery/">Stray Cats</a></div>
                   <div><a rel="noreferrer" itemProp="url" href="/photography/old-cat-gallery/">Old Cats</a></div>
-                  <span className="page-title"><a rel="noreferrer" itemProp="url" href="/photography/">All Galleries&nbsp;&rarr;</a></span>
+                  <span className="page__title"><a rel="noreferrer" itemProp="url" href="/photography/">All Galleries&nbsp;&rarr;</a></span>
                 </div>
             </div>
             <div>
-                <span className="page-title"><a rel="noreferrer" itemProp="url" href="/admin/index.html">Admin</a></span>
-                <div className="toc1-menu-popup list-unstyled">
-                   <div><a rel="noreferrer" itemProp="url" href="/admin/index.html">Admin Login</a></div>
-    		           <div><a rel="noreferrer" itemProp="url" href="/admin/admin/">Edit</a></div>              
+                <span className="page__title"><a rel="noreferrer" itemProp="url" href="/admin/index.html">Admin</a></span>
+                <div className="toc1-menu">
+                   <div><a rel="noreferrer" itemProp="url" href="/admin/index.html">Admin Login</a></div>              
                 </div>
             </div>
           </div>
