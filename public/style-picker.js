cb3ed6cc93cc90cff2e495303a605ce7f4353807
Donald Boulton <donaldboulton@gmail.com>
2019-01-01 16:55:35 -0600

    custom data  item values

--- public/style-picker.js
+++ public/style-picker.js
@@ -1,10 +1,8 @@
 $('#my-size').change(function() {
     $('#my-button').data('item-custom1-value', $(this).val());
 });
-$('#select-color').change(function() {
-    $('.snipcart-add-item').hide();    
-    var s = $('.snipcart-add-item[data-item-id="' + $(this).val() + '"]');
-    s.show();
+$('#my-color').change(function() {
+    $('#my-button').data('item-custom2-value', $(this).val());
 });
 $('#my-quantity').change(function() {
     $('#my-button').data('item-quantity', $(this).val());
