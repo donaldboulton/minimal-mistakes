8097a456f153b79db38e712e6486e5e27deee72d
Donald Boulton <donaldboulton@gmail.com>
2019-01-09 11:18:38 -0600

    reviews notify fix

--- build/js/staticman/staticman-reviews.js
+++ build/js/staticman/staticman-reviews.js
@@ -18,14 +18,14 @@
                 $('#review-form-submit').html('{{ "Submitted" }}');
                 $('.page__reviews-form .js-notice').removeClass('notice--danger');
                 $('.page__reviews-form .js-notice').addClass('notice--success');
-                showAlert('{{ site.data.ui-text[site.locale].review_success_msg | default: "Thanks for your Review! It should show up after CloudFlare Cache purge. about a minute." }}');
+                showAlert("Thanks for your Review! It should show up after CloudFlare Cache purge. about a minute.");
             },
             error(err) {
                 console.log(err);
                 $('#review-form-submit').html('{{ "Submit Review" }}');
                 $('.page__reviews-form .js-notice').removeClass('notice--success');
                 $('.page__reviews-form .js-notice').addClass('notice--danger');
-                showAlert('{{ site.data.ui-text[site.locale].review_error_msg | default: "Sorry, there was an error with your submission. Please make sure all required fields have been completed and try again." }}');
+                showAlert("Sorry, there was an error with your submission. Please make sure all required fields have been completed and try again.");
                 $(form).removeClass('disabled');
             },
         });
