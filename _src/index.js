e4e40590c094c39055c1cacfa52e2f5eba8350af
Donald Boulton <donaldboulton@gmail.com>
2019-01-21 15:14:20 -0600

    need bable pollyfill

--- _src/index.js
+++ _src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Todo from './lib/components/Todo';
 import AppNotify from './lib/components/AppNotify';
+import "babel-polyfill";
 import Dialog from './lib/components/Dialog';
+
 class App extends React.Component {
   render() {
     return (
